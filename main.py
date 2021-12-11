20ced2ce60549cf622567a24024c823a59bbb192
Владислав Хейнов <kheynov@Air-Vladislav.internal.skbrii.ru>
2021-12-11 17:07:27 +0700

    feat: add MVP code

---
+++ main.py
@@ -0,0 +1,39 @@
+from websocket import create_connection 
+import Adafruit_DHT as dht
+import time
+import json 
+
+register_message = {
+    "type": "register",
+    "payload": {
+        "name": "abobus",
+        "type": "dht"
+    }
+}
+
+server_url = "ws://192.168.2.37:3000/hub/ws"
+
+ws = create_connection(server_url)
+print("Registered with: " + json.dumps(register_message))
+ws.send(json.dumps(register_message))
+
+try:
+    while True:
+        humidity, temperature = dht.read_retry(11, 14)
+        data = {
+            "type": "data",
+            "payload": {
+                "humidity": humidity,
+                "temperature": temperature
+            }
+        }
+        print(json.dumps(data))
+        time.sleep(1)
+        ws.send(json.dumps(data))
+        # print("Humidity: " + str(humidity) + " | Temperature: " + str(temperature) + "\n")
+except:
+    print("Error occured")
+finally:
+    ws.close()
+    print("Exiting...")
+# ws = create_connection(server_url)
